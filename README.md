0cbcd77879dbc6e6332a4ea4ad5721c04cf11633
Andrei Babintsev <snakemgl.ukraine@gmail.com>
2019-02-04 16:55:19 +0200

    init commit 2

---
+++ README.md
@@ -0,0 +1,5 @@
+# Менеджер задач (Hexlet Project 4)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/46dcd469d60d008dc416/maintainability)](https://codeclimate.com/github/Snokke/project-lvl4-s371/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/46dcd469d60d008dc416/test_coverage)](https://codeclimate.com/github/Snokke/project-lvl4-s371/test_coverage)
+[![Build Status](https://travis-ci.org/Snokke/project-lvl4-s371.svg?branch=master)](https://travis-ci.org/Snokke/project-lvl4-s371)
---
+++ __tests__/app.test.js
@@ -0,0 +1,41 @@
+import request from 'supertest';
+// import matchers from 'jest-supertest-matchers';
+
+import app from '..';
+
+describe('requests', () => {
+  let server;
+
+  // beforeAll(() => {
+  //  expect.extend(matchers);
+  // });
+
+  beforeEach(() => {
+    server = app().listen();
+  });
+
+  it('GET 200', async () => {
+    await request.agent(server)
+      .get('/')
+      .expect(200);
+    // expect(res).toHaveHTTPStatus(200);
+    // const res = await request.agent(server)
+    //  .get('/');
+    // expect(res).toHaveHTTPStatus(200);
+  });
+
+  it('GET 404', async () => {
+    await request.agent(server)
+      .get('/wrong-path')
+      .expect(404);
+    // expect(res).toHaveHTTPStatus(404);
+    // const res = await request.agent(server)
+    //  .get('/wrong-path');
+    // expect(res).toHaveHTTPStatus(404);
+  });
+
+  afterEach((done) => {
+    server.close();
+    done();
+  });
+});
