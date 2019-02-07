1592a8fc749e7ba83600fe526f19e0addb00a127
Andrei Babintsev <snakemgl.ukraine@gmail.com>
2019-02-08 01:27:20 +0200

    update webpack config v2

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ import path from 'path';
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
+  entry: ['./src/index.js'],
   output: {
     path: path.join(__dirname, 'public', 'assets'),
     filename: 'main.js',
