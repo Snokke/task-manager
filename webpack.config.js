d9e1a1be938d7c6ac6ec5af45e024cd781221baf
Andrei Babintsev <snakemgl.ukraine@gmail.com>
2019-02-17 01:55:09 +0200

    fix bootstrap late loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const BitBarWebpackProgressPlugin = require('bitbar-webpack-progress-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 const path = require('path');
+
+// const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
@@ -21,16 +24,22 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader', 'postcss-loader'],
       },
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css',
+    }),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
       'window.jQuery': 'jquery',
     }),
     new BitBarWebpackProgressPlugin(),
+
   ],
 };
